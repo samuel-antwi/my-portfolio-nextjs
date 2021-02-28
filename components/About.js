090b96418b30065c93d9562fc40d8127aaf3b358
Samuel Antwi <samuelantwidev@gmail.com>
2021-02-28 11:51:14 +0000

    added contact-form and formspree cli

--- components/About.js
+++ components/About.js
@@ -32,9 +32,12 @@ const About = () => {
             hidden: { opacity: 0, scale: 0 },
           }}
           className='h-1 bg-gray-900 w-10 flex max-w-xs mx-auto'></motion.div>
-        <section className='sm:grid md:grid-cols-4 sm:grid-cols-2 max-w-5xl mx-auto py-20 gap-5'>
+        <section className='grid md:grid-cols-4 grid-cols-2 md:max-w-5xl mx-auto py-20 gap-5 space-y-4 md:space-y-0 px-6'>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
-            <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
+            <div
+              data-aos='slide-right'
+              data-aos-duration='1000'
+              className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
               <div className='relative'>
                 <FaLaptop size={50} className='text-gray-100' />
                 <FaMobile size={25} className='text-gray-100 absolute top-0 left-8' />
@@ -46,7 +49,10 @@ const About = () => {
             <p className='text-gray-500'>My layouts will work on any device, big or small..</p>
           </div>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
-            <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
+            <div
+              data-aos='slide-down'
+              data-aos-duration='1000'
+              className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
               <div>
                 <ImAlarm size={50} className='text-gray-100' />
               </div>
@@ -57,7 +63,10 @@ const About = () => {
             </p>
           </div>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
-            <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
+            <div
+              data-aos='slide-up'
+              data-aos-duration='1000'
+              className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
               <div>
                 <FaRegLightbulb size={50} className='text-gray-100' />
               </div>
@@ -70,7 +79,10 @@ const About = () => {
             </p>
           </div>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
-            <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xlrounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
+            <div
+              data-aos='slide-left'
+              data-aos-duration='1000'
+              className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xlrounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
               <div>
                 <ImRocket size={50} className='text-gray-100' />
               </div>
@@ -85,21 +97,31 @@ const About = () => {
           <div className='max-w-6xl mx-auto'>
             <div className='md:grid grid-cols-5 gap-20'>
               <div className='col-span-2'>
-                <Image
-                  className=' mb-4 '
-                  className='object-cover'
-                  src={'/images/sam5.png'}
-                  width={800}
-                  height={980}
-                />
-                <h1 className='md:text-2xl font-medium mb-3 text-center'>Who am I?</h1>
-                <p className='md:text-lg text-gray-600 text-center mb-1'>
+                <motion.div
+                  ref={ref}
+                  animate={controls}
+                  initial='hidden'
+                  transition={{ duration: 0.8 }}
+                  variants={{
+                    visible: { opacity: 1, scale: 1 },
+                    hidden: { opacity: 0, scale: 0 },
+                  }}>
+                  <Image
+                    className=' mb-4 '
+                    className='object-cover'
+                    src={'/images/sam5.png'}
+                    width={800}
+                    height={980}
+                  />
+                </motion.div>
+                <h1 className='md:text-2xl font-medium mb-3 text-center px-8'>Who am I?</h1>
+                <p className='md:text-lg text-gray-600 text-center mb-1 px-8 md:px-0'>
                   I'm a Front-End Developer based in Mansfield, Nottinghamshire. My goal is to use
                   mordern stacks to build amazing web application that can have real positive impact
                   in people's life without compromising their experience.
                 </p>
                 <a
-                  className='text-blue-400 hover:underline text-center block text-lg tracking-wider'
+                  className='text-blue-400 hover:underline text-center block text-lg tracking-wider mb-10'
                   href='/'>
                   Let work together
                 </a>
@@ -113,52 +135,18 @@ const About = () => {
                   visible: { opacity: 1, scale: 1 },
                   hidden: { opacity: 0, scale: 0 },
                 }}
-                className='col-span-3 space-y-3'>
+                className='col-span-3 space-y-3 px-8'>
                 <h1 className=' text-lg font-semibold tracking-wider'>Professional Skills</h1>
-                <div className='html text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold '>HTML</span>
-                  <span className='text-gray-700'> 80%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>CSS</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>JavaScript</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>React/Next</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Headless CMS</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Git</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Jira</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Scrum</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Agile</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>Firebase</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
-                <div className='css text-gray-100 px-2 py-1 flex justify-between'>
-                  <span className='font-semibold tracking-wider'>GraphQL</span>
-                  <span className='text-gray-700'> 70%</span>
-                </div>
+                <Skill skill='HTML' level='80%' className='html' />
+                <Skill skill='CSS' level='75%' className='css' />
+                <Skill skill='React/Next' level='75%' className='css' />
+                <Skill skill='Headless CMS' level='75%' className='css' />
+                <Skill skill='Git' level='75%' className='css' />
+                <Skill skill='Jira' level='75%' className='css' />
+                <Skill skill='Scrum' level='75%' className='css' />
+                <Skill skill='Agile' level='75%' className='css' />
+                <Skill skill='Firebase' level='75%' className='css' />
+                <Skill skill='GraphQL' level='75%' className='css' />
               </motion.div>
             </div>
           </div>
@@ -169,3 +157,12 @@ const About = () => {
 };
 
 export default About;
+
+export const Skill = ({ skill, level, className }) => {
+  return (
+    <div className={`${className} text-gray-100 px-2 py-1 flex justify-between`}>
+      <span className='font-semibold tracking-wider'>{skill}</span>
+      <span className='text-gray-700'> {level}</span>
+    </div>
+  );
+};
---
+++ components/Contact.jsx
@@ -0,0 +1,70 @@
+import { useForm, ValidationError } from '@formspree/react';
+
+const Contact = () => {
+  const [state, handleSubmit] = useForm('contactForm');
+
+  if (state.succeeded) {
+    return <div>Thank you for contacting me. I will be intouch very soon!</div>;
+  }
+
+  return (
+    <div className='bg-contact text-gray-100 min-h-screen py-20'>
+      <div className='max-w-xl mx-auto'>
+        <div data-aos='slide-right' data-aos-duration='1000'>
+          <h1 className='text-center uppercase md:text-3xl font-semibold tracking-widest mb-3'>
+            Contact
+          </h1>
+          <div className='h-1 bg-gray-50 w-16 flex max-w-xs mx-auto'></div>
+        </div>
+        <p
+          data-aos='slide-left'
+          data-aos-duration='1000'
+          className='py-10 text-secondary text-center'>
+          Have a question or want to work together?
+        </p>
+        <div data-aos='flip-right' data-aos-duration='1000'>
+          <form onSubmit={handleSubmit} className='space-y-1 text-gray-300'>
+            <div>
+              <input className='w-full p-2 bg-input' placeholder='Name' type='text' name='Name' />
+              <span className='text-red-200 text-xs'>
+                <ValidationError prefix='Name' field='Name' errors={state.errors} />
+              </span>
+            </div>
+            <div>
+              <input
+                className='w-full p-2 bg-input'
+                placeholder='Enter email'
+                type='text'
+                name='Email'
+              />
+              <span className='text-red-200 text-xs'>
+                <ValidationError prefix='Message' field='Message' errors={state.errors} />
+              </span>
+            </div>
+            <div>
+              <textarea
+                className='w-full p-2 bg-input'
+                placeholder='Your message'
+                name='Message'
+                id=''
+                cols='30'
+                rows='10'></textarea>
+              <span className='text-red-200 text-xs'>
+                <ValidationError prefix='Message' field='Message' errors={state.errors} />
+              </span>
+            </div>
+            <div className='flex justify-end'>
+              <button
+                disabled={state.submitting}
+                className='border-2 px-6 tracking-wider hover:bg-secondary hover:border-secondary uppercase py-2 transition duration-500 ease-in-out'>
+                {state.submitting ? ' Submiting' : 'Submit'}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
