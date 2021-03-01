85c8a27e6462954515cb26fd3f8cb391e3f2aef7
Samuel Antwi <samuelantwidev@gmail.com>
2021-03-01 14:38:14 +0000

    created side nav

--- components/Nav.js
+++ components/Nav.js
@@ -4,38 +4,43 @@ import Link from 'next/link';
 import { VscChevronLeft, VscChevronRight, VscMenu, VscClose } from 'react-icons/vsc';
 import { useRouter } from 'next/router';
 import { Sling as Hamburger } from 'hamburger-react';
+import SideNav from './SideNav';
 
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <Styles className='bg-nav text-gray-200 p-4 sticky top-0 border-b-4 border-secondary z-10'>
-      <div className='hidden md:block'>
-        <ul className='flex items-center space-x-6  max-w-5xl mx-auto'>
-          <li>
-            <a className='uppercase tracking-widest px-4 py-2 text-sm ' href='/'>
-              Home
-            </a>
-          </li>
-          <li>
-            <a className='uppercase tracking-wider px-4 py-2 text-sm' href='/'>
-              About
-            </a>
-          </li>
-          <li>
-            <a className='uppercase tracking-widest px-4 py-2 text-sm' href='/'>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a className='uppercase tracking-widest px-4 py-2 text-sm' href='/'>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className='md:hidden'>
-        <Hamburger duration={0.8} size={28} />
-      </div>
-    </Styles>
+    <>
+      <Styles className='sticky top-0 bg-nav text-gray-200 p-4  border-b-4 border-secondary z-20 '>
+        <div className='hidden md:block'>
+          <ul className='flex items-center space-x-6  max-w-5xl mx-auto'>
+            <li>
+              <a className='uppercase tracking-widest px-4 py-2 text-sm ' href='/'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className='uppercase tracking-wider px-4 py-2 text-sm' href='/'>
+                About
+              </a>
+            </li>
+            <li>
+              <a className='uppercase tracking-widest px-4 py-2 text-sm' href='/'>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a className='uppercase tracking-widest px-4 py-2 text-sm' href='/'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='md:hidden'>
+          <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} size={28} />
+        </div>
+      </Styles>
+      {isOpen && <SideNav isOpen={isOpen} setOpen={setOpen} />}
+    </>
   );
 };
 
