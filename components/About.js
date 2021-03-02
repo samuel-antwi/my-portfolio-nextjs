6f1c0aca6dee5b10b7946749e43537196ab07071
Samuel Antwi <samuelantwidev@gmail.com>
2021-03-02 08:45:33 +0000

    changed site background to dark mode

--- components/About.js
+++ components/About.js
@@ -18,7 +18,7 @@ const About = () => {
   }, [controls, inView]);
 
   return (
-    <div className='about min-h-screen text-gray-700'>
+    <div className='about min-h-screen bg-primary text-gray-200'>
       <div className='py-20'>
         <h1 className='text-center uppercase md:text-3xl font-semibold tracking-widest mb-3'>
           About
@@ -32,7 +32,7 @@ const About = () => {
             visible: { opacity: 1, scale: 1 },
             hidden: { opacity: 0, scale: 0 },
           }}
-          className='h-1 bg-gray-900 w-10 flex max-w-xs mx-auto'></motion.div>
+          className='h-1 bg-gray-200 w-10 flex max-w-xs mx-auto'></motion.div>
         <section className='grid md:grid-cols-4 grid-cols-2 md:max-w-5xl mx-auto py-20 gap-5 space-y-4 md:space-y-0 px-6'>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
             <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
@@ -41,10 +41,10 @@ const About = () => {
                 <FaMobile size={25} className='text-gray-100 absolute top-0 left-8' />
               </div>
             </div>
-            <h1 className='mb-2 md:text-2xl font-semibold text-gray-600 tracking-wider'>
+            <h1 className='mb-2 md:text-2xl font-semibold text-gray-100 tracking-wider'>
               Responvie
             </h1>
-            <p className='text-gray-500'>
+            <p className='text-gray-300'>
               We are not in 90's. Don't pay extra for responsive website.
             </p>
           </div>
@@ -54,8 +54,8 @@ const About = () => {
                 <ImAlarm size={50} className='text-gray-100' />
               </div>
             </div>
-            <h1 className='mb-2 md:text-2xl font-semibold text-gray-600 tracking-wider'>Fast</h1>
-            <p className='text-gray-500'>Fast load times. This is the reason I love NextJS</p>
+            <h1 className='mb-2 md:text-2xl font-semibold text-gray-100 tracking-wider'>Fast</h1>
+            <p className='text-gray-300'>Fast load times. This is the reason I love NextJS</p>
           </div>
           <div className='col-span-1 text-center flex flex-col justify-center justify-items-center items-center'>
             <div className='h-24 w-24 mb-3 rounded-tl-3xl rounded-br-3xl bg-secondary flex justify-center justify-items-center items-center'>
@@ -63,10 +63,10 @@ const About = () => {
                 <FaRegLightbulb size={50} className='text-gray-100' />
               </div>
             </div>
-            <h1 className='mb-2 md:text-2xl font-semibold text-gray-600 tracking-wider'>
+            <h1 className='mb-2 md:text-2xl font-semibold text-gray-100 tracking-wider'>
               Intuitive
             </h1>
-            <p className='text-gray-500'>
+            <p className='text-gray-300'>
               Strong preference for easy to use, intuitive UX/UI. Dynamic .
             </p>
           </div>
@@ -76,72 +76,64 @@ const About = () => {
                 <ImRocket size={50} className='text-gray-100' />
               </div>
             </div>
-            <h1 className='mb-2 md:text-2xl font-semibold text-gray-600 tracking-wider'>Dynamic</h1>
-            <p className='text-gray-500'>
+            <h1 className='mb-2 md:text-2xl font-semibold text-gray-100 tracking-wider'>Dynamic</h1>
+            <p className='text-gray-300'>
               Websites don't have to be static. My goal is to make pages come to life
             </p>
           </div>
         </section>
-        <section className=''>
-          <div className='max-w-6xl mx-auto'>
-            <div className='md:grid grid-cols-5 gap-20'>
-              <div className='col-span-2'>
-                <motion.div
-                  ref={ref}
-                  animate={controls}
-                  initial='hidden'
-                  transition={{ duration: 0.8 }}
-                  variants={{
-                    visible: { opacity: 1, scale: 1 },
-                    hidden: { opacity: 0, scale: 0 },
-                  }}>
-                  <Image
-                    className='object-cover'
-                    src={'/images/sam5.png'}
-                    width={800}
-                    height={980}
-                    priority
-                  />
-                </motion.div>
-                <h1 className='md:text-2xl text-lg  font-semibold text-gray-700 py-4 text-center px-8'>
-                  Who am I?
-                </h1>
-                <p className='md:text-lg text-gray-600 text-center mb-1 px-8 md:px-0'>
-                  I'm a Front-End Developer based in Mansfield, Nottinghamshire. My goal is to use
-                  mordern stacks to build amazing web application that can have real positive impact
-                  in people's life without compromising their experience.
-                </p>
-                <Link
-                  to='contact'
-                  className='cursor-pointer text-blue-400 hover:underline text-center block text-lg tracking-wider mb-10'>
-                  Let work together
-                </Link>
-              </div>
-              <motion.div
-                ref={ref}
-                animate={controls}
-                initial='hidden'
-                transition={{ duration: 0.8 }}
-                variants={{
-                  visible: { opacity: 1, scale: 1 },
-                  hidden: { opacity: 0, scale: 0 },
-                }}
-                className='col-span-3 space-y-3 px-8'>
-                <h1 className=' text-lg font-semibold tracking-wider'>Professional Skills</h1>
-                <Skill skill='HTML' />
-                <Skill skill='CSS' />
-                <Skill skill='JavaScript' />
-                <Skill skill='React/Next' />
-                <Skill skill='Headless CMS' />
-                <Skill skill='Git' />
-                <Skill skill='Jira' />
-                <Skill skill='Scrum' />
-                <Skill skill='Agile' />
-                <Skill skill='Firebase' />
-                <Skill skill='GraphQL' />
-                <Skill skill='REST' />
-              </motion.div>
+        <section className='md:grid grid-cols-4 gap-10 max-w-4xl mx-auto'>
+          <motion.div
+            className='col-span-2'
+            ref={ref}
+            animate={controls}
+            initial='hidden'
+            transition={{ duration: 0.8 }}
+            variants={{
+              visible: { opacity: 1, scale: 1 },
+              hidden: { opacity: 0, scale: 0 },
+            }}>
+            <Image
+              className='object-cover'
+              src={'/images/sam5.png'}
+              width={800}
+              height={980}
+              priority
+            />
+          </motion.div>
+          <div className='col-span-2 px-8 md:px-0'>
+            <h1 className='md:text-2xl text-lg tracking-wider  font-semibold text-gray-100 py-4'>
+              Who am I?
+            </h1>
+            <div className='md:text-lg text-gray-300  mb-1  tracking-wide font-raleway'>
+              <p>
+                I'm a Front-End Developer based in Mansfield, Nottinghamshire. My goal is to use
+                mordern stacks to build amazing web application that can have real positive impact
+                in people's life without compromising their experience.
+              </p>
+              <p>
+                I have a genuine passion for tech and I always do my best to keep up with the latest
+                technologies and tools that help frontend developers to build a fully-fledged web
+                application.
+              </p>
+              <p>If I am not writing code, it means I am busy DJing. 💿 🎵</p>
+              <ul className='pt-5 flex  flex-wrap text-secondary'>
+                <li>HTML/CSS</li>
+                <li>JavaScript</li>
+                <li>React/NextJS</li>
+                <li>Headless CMS</li>
+                <li>Firebase</li>
+                <li>Responsive Design</li>
+                <li>Git</li>
+                <li>Jira</li>
+                <li>Agile</li>
+              </ul>
             </div>
+            <Link
+              to='contact'
+              className='cursor-pointer text-blue-400 hover:underline pt-3 block text-lg tracking-wider mb-10'>
+              Let work together
+            </Link>
           </div>
         </section>
       </div>
