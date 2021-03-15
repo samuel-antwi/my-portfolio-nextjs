ee779b1ae39fb91e4efacfd4af1ec7339f4004aa
Samuel Antwi <samuelantwidev@gmail.com>
2021-03-15 23:31:44 +0000

    update: side-nav

--- components/SideNav.js
+++ components/SideNav.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-scroll';
+import styled from 'styled-components';
 
 const SideNav = ({ isOpen, setOpen }) => {
   return (
@@ -7,7 +8,7 @@ const SideNav = ({ isOpen, setOpen }) => {
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
-      className='sticky top-20 bg-primary h-64 text-gray-100 w-full p-5 flex flex-col space-y-4 md:hidden z-10'>
+      className='wrapper absolute shadow-md tracking-widest text-lg  bg-primary h-96 text-gray-100 w-full  p-5 flex flex-col space-y-4 md:hidden z-10'>
       <Links setOpen={setOpen} link='home'>
         Home
       </Links>
@@ -28,10 +29,7 @@ export default SideNav;
 
 const Links = ({ link, children, setOpen }) => {
   return (
-    <Link
-      onClick={() => setOpen(false)}
-      to={link}
-      className='uppercase cursor-pointer text-gray-300 tracking-widest hover:text-primary text-sm'>
+    <Link onClick={() => setOpen(false)} to={link}>
       {children}
     </Link>
   );
